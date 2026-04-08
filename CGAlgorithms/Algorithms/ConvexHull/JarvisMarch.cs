9b9cb152b6d84794aeeee0b893feb3daddf09f0e
randern212 <randern212@gmail.com>
2026-04-08 08:18:20 +0200

    basic logic still needs correction tho

--- CGAlgorithms/Algorithms/ConvexHull/JarvisMarch.cs
+++ CGAlgorithms/Algorithms/ConvexHull/JarvisMarch.cs
@@ -11,7 +11,41 @@ namespace CGAlgorithms.Algorithms.ConvexHull
     {
         public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
         {
+            //search for the point with the least x-axis value
+            points.OrderBy(p => p.X);
+            Point startingPoint = points.First();
 
+            List<Point> hull = new List<Point>();
+            hull.Add(startingPoint);
+            Point currentPoint = startingPoint;
+
+            while (true)
+            {
+                Point nextPoint = null;
+                foreach (Point p in points)
+                {
+
+                    if (nextPoint == null)
+                        nextPoint = p;
+
+                    Line currentToNextLine = new Line(currentPoint, nextPoint);
+                    Line currentToPLine = new Line(currentPoint, p);
+                    Point cnVector = HelperMethods.GetVector(currentToNextLine);
+                    Point cpVector = HelperMethods.GetVector(currentToPLine);
+
+                    Enums.TurnType currentToNextTurn = HelperMethods.CheckTurn(cnVector, cpVector);
+
+                    if (currentToNextTurn == Enums.TurnType.Left)
+                    {
+                        currentPoint = nextPoint;
+                        hull.Add(currentPoint);
+                    }
+                }
+    
+                //if current p == p then terminate loop
+                if (nextPoint == startingPoint);
+                    break;
+            }
         }
 
         public override string ToString()
